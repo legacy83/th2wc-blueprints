6dcccf1541a3a3153ebc51d6589ef4e343fe9891
Thiago Senna <trsenna@gmail.com>
2015-06-03 00:30:01 -0300

    minimal picturesque14 version for WooCommerce

---
+++ themes/picturesque14/woocommerce.php
@@ -0,0 +1,22 @@
+<?php get_header(); // Loads the header.php template. ?>
+
+<?php do_atomic( 'before_content' ); // picturesque_before_content ?>
+
+    <div id="content">
+
+        <?php do_atomic( 'open_content' ); // picturesque_open_content ?>
+
+        <div class="hfeed">
+
+            <?php woocommerce_content(); ?>
+
+        </div>
+        <!-- .hfeed -->
+
+        <?php do_atomic( 'close_content' ); // picturesque_close_content ?>
+
+    </div><!-- #content -->
+
+<?php do_atomic( 'after_content' ); // picturesque_after_content ?>
+
+<?php get_footer(); // Loads the footer.php template. ?>
