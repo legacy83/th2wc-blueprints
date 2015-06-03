9dead77e7d3afcda21068fac080c4d64d1b471e5
Thiago Senna <trsenna@gmail.com>
2015-06-03 00:39:39 -0300

    minimal unique30 version for WooCommerce

---
+++ themes/unique30/woocommerce.php
@@ -0,0 +1,9 @@
+<?php get_header(); // Loads the header.php template. ?>
+
+    <div id="content" class="hfeed">
+
+        <?php woocommerce_content(); ?>
+
+    </div><!-- #content -->
+
+<?php get_footer(); // Loads the footer.php template. ?>
