abddd3d2bcc886b6f3b777892f23678331e0739d
Thiago Senna <trsenna@gmail.com>
2015-06-02 23:00:01 -0300

    initial my-life31

---
+++ themes/my-life31/functions.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * (To women begging the king to spare their lives) "Ha-ha these humans are
+ * definitely foolish creatures.  Think as hard as those weak brains of
+ * yours can manage. Do you humans ever listen to the cries of mercy coming
+ * from pigs and cows you slaughter?" ~ Meruem | Hunter x Hunter
+ *
+ * @package    MyLife31
+ * @author     Thiago Senna <thiago@thremes.com.br>
+ * @copyright  Copyright (c) 2015, Thiago Senna
+ * @license    http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ */
+
+add_action( 'after_setup_theme', 'mylife31_setup' );
+
+/**
+ * The Theme Setup
+ */
+function mylife31_setup()
+{
+    // TODO - Here goes your custom functionality...
+}
