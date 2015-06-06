95d650b7860dffe09fc37d8413e35e79784ebe58
Thiago Senna <trsenna@gmail.com>
2015-06-06 16:04:07 -0300

    a better woocommerce.php template for prototype36

--- themes/prototype36/functions.php
+++ themes/prototype36/functions.php
@@ -12,24 +12,11 @@
  */
 
 add_action( 'after_setup_theme', 'prototype36_setup' );
-add_action( 'after_setup_theme', 'prototype36_wc_setup' );
 
 /**
  * The Theme Setup
  */
 function prototype36_setup()
-{
-    /*
-     * scripts and styles
-     */
-
-    add_action( 'wp_enqueue_scripts', 'prototype36_enqueue_styles' );
-}
-
-/**
- * The WC Setup
- */
-function prototype36_wc_setup()
 {
     /*
      * declare WooCommerce support
@@ -38,10 +25,10 @@ function prototype36_wc_setup()
     add_theme_support( 'woocommerce' );
 
     /*
-     * no wc pagination
+     * scripts and styles
      */
 
-    remove_action( 'woocommerce_after_shop_loop', 'woocommerce_pagination', 10 );
+    add_action( 'wp_enqueue_scripts', 'prototype36_enqueue_styles' );
 }
 
 /**
--- themes/prototype36/woocommerce.php
+++ themes/prototype36/woocommerce.php
@@ -8,7 +8,33 @@
 
         <div class="hfeed">
 
-            <?php woocommerce_content(); ?>
+            <?php if ( is_shop() || is_product_taxonomy() ) : ?>
+
+                <div class="loop-meta">
+
+                    <h1 class="loop-title"><?php woocommerce_page_title(); ?></h1>
+
+                    <div class="loop-description">
+                        <?php
+                        ob_start();
+                        woocommerce_product_archive_description();
+                        woocommerce_taxonomy_archive_description();
+                        echo wpautop( trim( strip_tags( ob_get_clean() ) ) );
+                        ?>
+                    </div>
+
+                </div>
+                <!-- .loop-meta -->
+
+            <?php endif; ?>
+
+            <?php
+            add_filter( 'woocommerce_show_page_title', '__return_false' );
+            remove_action( 'woocommerce_archive_description', 'woocommerce_taxonomy_archive_description', 10 );
+            remove_action( 'woocommerce_archive_description', 'woocommerce_product_archive_description', 10 );
+            remove_action( 'woocommerce_after_shop_loop', 'woocommerce_pagination', 10 );
+            woocommerce_content();
+            ?>
 
         </div>
         <!-- .hfeed -->
