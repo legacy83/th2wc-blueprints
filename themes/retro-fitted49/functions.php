cea785a61aff6c3109a1ea511b0b54649f58846f
Thiago Senna <trsenna@gmail.com>
2015-06-06 16:10:05 -0300

    a better woocommerce.php template for retro-fitted49

--- themes/retro-fitted49/functions.php
+++ themes/retro-fitted49/functions.php
@@ -12,24 +12,11 @@
  */
 
 add_action( 'after_setup_theme', 'retrofitted49_setup' );
-add_action( 'after_setup_theme', 'retrofitted49_wc_setup' );
 
 /**
  * The Theme Setup
  */
 function retrofitted49_setup()
-{
-    /*
-     * scripts and styles
-     */
-
-    add_action( 'wp_enqueue_scripts', 'retrofitted49_enqueue_styles' );
-}
-
-/**
- * The WC Setup
- */
-function retrofitted49_wc_setup()
 {
     /*
      * declare WooCommerce support
@@ -38,10 +25,10 @@ function retrofitted49_wc_setup()
     add_theme_support( 'woocommerce' );
 
     /*
-     * no wc pagination
+     * scripts and styles
      */
 
-    remove_action( 'woocommerce_after_shop_loop', 'woocommerce_pagination', 10 );
+    add_action( 'wp_enqueue_scripts', 'retrofitted49_enqueue_styles' );
 }
 
 /**
